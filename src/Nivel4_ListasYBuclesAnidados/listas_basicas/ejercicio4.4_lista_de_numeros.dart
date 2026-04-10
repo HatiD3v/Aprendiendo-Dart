fa491e464be55bef0a8afd2ece6147470f7854f3
HatiD3v <edwinmg27@gmail.com>
2026-04-09 23:40:09 -0400

    add: [ejercicio 4.4 lista de numeros e imprimir por indice ]

---
+++ src/Nivel4_ListasYBuclesAnidados/listas_basicas/ejercicio4.4_lista_de_numeros.dart
@@ -0,0 +1,7 @@
+// Crea una List<int> con cinco números y muestra el primero y el último.
+void main() {
+  List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+  print(
+    'El primer y ultimo numero de la lista son: Primero:${numbers[0]} Ultimo:${numbers[7]}',
+  );
+}
