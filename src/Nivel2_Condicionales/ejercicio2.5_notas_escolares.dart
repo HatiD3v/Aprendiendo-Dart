42be93adb79f0e2d4d34327741352f36e1058903
HatiD3v <edwinmg27@gmail.com>
2026-04-12 18:43:14 -0400

    add: [diagrama de flujo del ejercicio condicional 2.5 y ejercicio 2.5 nota escolar]

---
+++ src/Nivel2_Condicionales/ejercicio2.5_notas_escolares.dart
@@ -0,0 +1,11 @@
+void main() {
+  int calification = 15;
+  if (calification >= 10) {
+    print('Aprobaste con la calificacion de: $calification');
+  } else {
+    print('Reprobaste con la nota de: $calification');
+  }
+}
+//Todo: Dado un int que representa una calificación (0–20), muestra “Aprobado” si es mayor o igual que 10, de lo contrario “Reprobado”.
+/// Se inicializo la variable calification con un valor de 15.
+/// declaramos una estructura condicional primeramente valida si la condicion de calification es mayor o igual a 15 imprime aprobado si no reprobado.
