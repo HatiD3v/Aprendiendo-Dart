b231505c86bb539c9351c3a76280a5743b7f3fc3
HatiD3v <edwinmg27@gmail.com>
2026-04-12 19:11:18 -0400

    add: [diagrama de flujo de ejercicio 2.6 numero par e impar y ejercicio ejercicio 2.6 ]

---
+++ src/Nivel2_Condicionales/ejercicio2.6_verificar_numero_parImpar.dart
@@ -0,0 +1,12 @@
+void main() {
+  int parImpar = 4;
+  if (parImpar % 2 == 0) {
+    print('El numero es par: $parImpar');
+  } else {
+    print('El numero es impar: $parImpar');
+  }
+}
+
+//Todo:Dado un número entero, determina si es par o impar.
+/// declaramos e inicializamos la variable parImpar con algun numero de preferencia.
+/// declaramos la condicional if else validando si el numero de la variable parImpar deja residuo al dividirla entre 2, si no deja residuo el numero es par, pero si deja residuo el numero es impar
