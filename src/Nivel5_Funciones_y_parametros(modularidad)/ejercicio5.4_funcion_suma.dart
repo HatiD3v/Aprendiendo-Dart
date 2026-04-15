a9685565e8e3cf8076a288b5952d3d7f1233380e
HatiD3v <edwinmg27@gmail.com>
2026-04-15 19:58:35 -0400

    add [ejercicio de funciones 5.4 funcion de suma ]

---
+++ src/Nivel5_Funciones_y_parametros(modularidad)/ejercicio5.4_funcion_suma.dart
@@ -0,0 +1,11 @@
+void main() {
+  int sumaTotal = suma(10, 5);
+  print(sumaTotal);
+  print(suma(2, 3));
+  print(suma(20, 453));
+  print(suma(20234, 21453));
+}
+
+int suma(int a, int b) {
+  return a + b;
+}
