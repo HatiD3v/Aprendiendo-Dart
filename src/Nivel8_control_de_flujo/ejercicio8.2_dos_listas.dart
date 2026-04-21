2f847c5c99436cad4f0aece9902ec208c7977d84
edwin <edwinmg27@gmail.com>
2026-04-21 17:32:56 -0400

    add: [ejercicio8.2 dos list]

---
+++ src/Nivel8_control_de_flujo/ejercicio8.2_dos_listas.dart
@@ -0,0 +1,4 @@
+void main() {
+  //Dada una lista de enteros, separa en dos listas: una con números positivos y otra con negativos (incluye cero donde creas lógico, pero sé consistente).
+  // Pista: en cada elemento, usa un if para decidir en qué lista colocar el valor.
+}
