a2346b868f56e0fd5ec26aaaef4a54742dcb15f8
HatiD3v <edwinmg27@gmail.com>
2026-04-21 21:04:21 -0400

    fix: [diagrama de flujo y ejercicio 8.2 creacion de dos listas]

--- src/Nivel8_control_de_flujo/ejercicio8.2_dos_listas.dart
+++ src/Nivel8_control_de_flujo/ejercicio8.2_dos_listas.dart
@@ -1,4 +1,17 @@
 void main() {
   //Dada una lista de enteros, separa en dos listas: una con números positivos y otra con negativos (incluye cero donde creas lógico, pero sé consistente).
   // Pista: en cada elemento, usa un if para decidir en qué lista colocar el valor.
+  List<int> integers = [1, 2, -3, -4, 0, 7, -8, 6, -10];
+  List<int> positivo = [];
+  List<int> negativo = [];
+
+  for (int integer in integers) {
+    if (integer >= 0) {
+      positivo.add(integer);
+    } else {
+      negativo.add(integer);
+    }
+  }
+  print('numeros positivos $positivo');
+  print('numeros negativos $negativo');
 }
